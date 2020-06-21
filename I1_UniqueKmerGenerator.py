d3f812e2f153e2a8017c8c589e74c9f13160fa59
ChongLC <51225708+ChongLC@users.noreply.github.com>
2020-06-21 19:49:54 +0800

    Add files via upload

---
+++ I1_UniqueKmerGenerator.py
@@ -0,0 +1,38 @@
+from Bio import SeqIO
+from concurrent.futures import ProcessPoolExecutor
+import math
+
+fileA = list(SeqIO.parse("inputfile.fas","fasta"))
+file_id = "Output_kmers"
+
+def generate_kmers(start, end):
+	for record in fileA[start:end]:
+		nr_sequence = record.seq
+		seq_len = len(nr_sequence)
+		kmer = 9
+		count = 0
+		temp = []
+		for seq in list(range(seq_len-(kmer-1))):
+			count += 1
+			my_kmer = (nr_sequence[seq:seq+kmer])
+			temp.append(str(my_kmer))
+		with open(file_id, 'a') as f:
+			f.writelines("%s\n" % kmer for kmer in temp)
+
+if __name__ == '__main__':
+  n = len(fileA)
+  pool = ProcessPoolExecutor(14)
+  futures = []
+  perCPUSize = math.ceil(n/14)
+  for i in range(0,14):
+  	futures.append(pool.submit(generate_kmers, i * perCPUSize, (i+1) * perCPUSize))
+
+file_id = "Output_kmers"
+lines_seen = set ()
+
+outfile = open("nr_kmers", "w")
+for line in open("Output_kmers", "r"):
+        if line not in lines_seen: 
+                outfile.write(line)
+                lines_seen.add(line)
+outfile.close()
---
+++ I2_MinSetGenerator.py
@@ -0,0 +1,51 @@
+from Bio import SeqIO
+import pandas as pd
+
+fileA = list(SeqIO.parse("inputfile.fas","fasta"))
+fileB = pd.read_csv("nr_kmers",header = None)
+
+a = 0
+while len(fileB) != 0: 
+    output = open("matchingOut"+str(a)+".txt","w")
+    n = len(fileA)
+    for i in range (0,n):
+            kmercount = 0
+            fullseq = fileA[i].seq
+            for kmer in fileB[0]:
+                    if (fullseq.count(kmer) == 1):
+                            kmercount+=1
+            output.write(fileA[i].id + ';' + str(kmercount) + '\n')
+
+    output.close()
+
+    unsortedcp_fileC = pd.read_csv("matchingOut"+str(a)+".txt",header=None)
+    output = open("sortedcplist_fileD"+str(a)+".txt","w")
+
+    split_data = pd.DataFrame(unsortedcp_fileC[0].str.split(';',1).tolist(), columns=['acc_no','cp_count'])
+    split_data.cp_count = pd.to_numeric(split_data.cp_count)
+    sortedcplist_fileD = split_data.sort_values(by='cp_count',ascending=False)
+    output.write(sortedcplist_fileD.to_string())
+
+    output.close()
+
+    highestCPSeq = sortedcplist_fileD.iloc[0,0]
+    fileZ = open("fileZ.txt","a") # fileZ is an example of output, similar with provided exampleoutput.txt
+    for seq in fileA:
+            if(seq.id == highestCPSeq):
+                fullseq_highestCPid = seq.id
+                fileZ.write(seq.id + '\n')
+                fullseq_highestCP = seq.seq
+
+    fileZ.close()
+
+    fileY = open("fileY"+str(a)+".txt","w")
+    len(fileB[0])
+    for index, kmer in enumerate(fileB[0]):
+        if (kmer in fullseq_highestCP):
+            fileY.write(kmer + '\n')
+            fileB.drop(index, inplace = True)
+
+    fileY.close()
+    fileB.reset_index(drop = True, inplace = True)
+    fileB.to_csv("fileX"+str(a), index = None, header = None)
+    a += 1
